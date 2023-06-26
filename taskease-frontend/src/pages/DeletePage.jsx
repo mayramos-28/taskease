9358c1928fb90f3705bab0fac09dc9130b2cf4a1
mayra28mos <mayra28mos@gmail.com>
2023-06-26 17:17:42 +0200

    modify

--- taskease-frontend/src/pages/DeletePage.jsx
+++ taskease-frontend/src/pages/DeletePage.jsx
@@ -69,7 +69,7 @@ export const DeletePage = () => {
                   </div>
                   <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                     <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
-                      Deactivate account
+                     Eliminar tarea
                     </Dialog.Title>
                     <div className="mt-2">
                       <p className="text-sm text-gray-500">
